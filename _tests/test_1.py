ca5a5f8c7eb7b694bb9f22bf84e0f9b699814ed3
k.d <daniloff.kostyan@yandex.ru>
2023-03-29 03:42:09 +0300

    3

--- _tests/test_1.py
+++ _tests/test_1.py
@@ -3,7 +3,7 @@ from hexlet_code.scripts import gendiff
 
 def test_generate_diff():
     right_result = open('_tests/fixtures/rezult_f1_f2.json').read()
-    different = gendiff.generate_diff('_tests/fixtures/file1.json',
-                                      '_tests/fixtures/file2.json')
+    different = gendiff.generate_diff('_tests/fixtures/file_1.json',
+                                      '_tests/fixtures/file_2.json')
     assert type(different) == str
     assert different == right_result

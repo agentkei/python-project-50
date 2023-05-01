4b71e605db812123eb0f29d035b9ccc8f3cbd440
lilpep8 <daniloff.konstantin2016@yandex.ru>
2023-05-01 14:45:52 +0300

    @param_tests linter

--- _tests/test_all.py
+++ _tests/test_all.py
@@ -9,11 +9,25 @@ json_rez = open('_tests/fixtures/format_json.json').read()
 
 
 @pytest.mark.parametrize("file1, file2, rez", [
-    ('_tests/fixtures/flat_file1.json', '_tests/fixtures/flat_file2.json', flat_rez),
-    ('_tests/fixtures/flat_file1.yaml', '_tests/fixtures/flat_file2.yaml', flat_rez),
-    ('_tests/fixtures/rec_file1.json', '_tests/fixtures/rec_file2.json', rec_rez),
-    ('_tests/fixtures/rec_file1.yml', '_tests/fixtures/rec_file2.yml', rec_rez),
-    ('_tests/fixtures/rec_file1.yml', '_tests/fixtures/rec_file2.json', rec_rez)
+    ('_tests/fixtures/flat_file1.json',
+     '_tests/fixtures/flat_file2.json',
+        flat_rez),
+
+    ('_tests/fixtures/flat_file1.yaml',
+     '_tests/fixtures/flat_file2.yaml',
+        flat_rez),
+
+    ('_tests/fixtures/rec_file1.json',
+     '_tests/fixtures/rec_file2.json',
+        rec_rez),
+
+    ('_tests/fixtures/rec_file1.yml',
+     '_tests/fixtures/rec_file2.yml',
+        rec_rez),
+
+    ('_tests/fixtures/rec_file1.yml',
+     '_tests/fixtures/rec_file2.json',
+        rec_rez)
 ])
 def test_generate_diff(file1, file2, rez):
     different = generate_diff(file1, file2)
@@ -25,11 +39,11 @@ parametrizer = pytest.mark.parametrize("file1, file2, rez, format", [
     ('_tests/fixtures/rec_file1.json',
      '_tests/fixtures/rec_file2.yml',
      plain_rez,
-    "plain"),
+     "plain"),
     ('_tests/fixtures/rec_file1.yml',
      '_tests/fixtures/rec_file2.json',
      json_rez,
-    "json")
+     "json")
 ])
 
 

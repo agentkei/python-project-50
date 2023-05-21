13691a2f790bcc5f0067dbd0fbcc0c6d20288bf4
lilpep8 <daniloff.konstantin2016@yandex.ru>
2023-05-21 17:27:06 +0300

    del hex_code

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,33 +1,34 @@
 import pytest
 from gendiff.generate_diff import generate_diff
+import os
 
 
-flat_rez = open('tests/fixtures/right_rez_flat_file.json').read()
-rec_rez = open('tests/fixtures/right_rec_rez_file.txt').read()
-plain_rez = open('tests/fixtures/plain_result.txt').read()
-json_rez = open('tests/fixtures/format_json.json').read()
+flat_rez = open('right_rez_flat_file.json').read()
+rec_rez = open('right_rec_rez_file.txt').read()
+plain_rez = open('plain_result.txt').read()
+json_rez = open('format_json.json').read()
 
 
 @pytest.mark.parametrize("file1, file2, rez", [
-    ('tests/fixtures/flat_file1.json',
-     'tests/fixtures/flat_file2.json',
+    ('flat_file1.json',
+     'flat_file2.json',
         flat_rez),
 
-    ('tests/fixtures/flat_file1.yaml',
-     'tests/fixtures/flat_file2.yaml',
+    ('flat_file1.yaml',
+     'flat_file2.yaml',
         flat_rez),
 
-    ('tests/fixtures/rec_file1.json',
-     'tests/fixtures/rec_file2.json',
+    ('rec_file1.json',
+     'rec_file2.json',
 
         rec_rez),
 
-    ('tests/fixtures/rec_file1.yml',
-     'tests/fixtures/rec_file2.yml',
+    ('rec_file1.yml',
+     'rec_file2.yml',
         rec_rez),
 
-    ('tests/fixtures/rec_file1.yml',
-     'tests/fixtures/rec_file2.json',
+    ('rec_file1.yml',
+     'rec_file2.json',
         rec_rez)
 ])
 def test_generate_diff(file1, file2, rez):
@@ -37,12 +38,13 @@ def test_generate_diff(file1, file2, rez):
 
 
 parametrizer = pytest.mark.parametrize("file1, file2, rez, format", [
-    ('tests/fixtures/rec_file1.json',
-     'tests/fixtures/rec_file2.yml',
+    ('rec_file1.json',
+     'rec_file2.yml',
      plain_rez,
      "plain"),
-    ('tests/fixtures/rec_file1.yml',
-     'tests/fixtures/rec_file2.json',
+
+    ('rec_file1.yml',
+     'rec_file2.json',
      json_rez,
      "json")
 ])
